b0fedebde962282595707c26c84a53b749741d15
Leonardo Cotta <leoabreucotta@gmail.com>
2025-02-24 18:58:27 -0500

    fix dataset

--- dataset.py
+++ dataset.py
@@ -17,9 +17,6 @@ class Dataset:
         if not isinstance(self.X_col, str) or self.X_col not in FINGERPRINT_TYPES:
             raise ValueError(f"X_col must be one of {FINGERPRINT_TYPES}")
 
-        if not self.test and self.y_col != 'Label':
-            raise ValueError("y_col must be 'Label' for binary classification")
-
         df = pd.read_parquet(self.filename, columns=[self.X_col, self.y_col])
         self.y = df[self.y_col].values
         self.X = np.vstack([np.fromstring(x, sep=',') for x in df[self.X_col].values])
